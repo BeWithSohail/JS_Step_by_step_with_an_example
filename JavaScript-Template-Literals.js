1c20d41a80a9cbfec85e638ac176aae76cc74f7f
Md Sohail <sohailalam6027@gmail.com>
2023-01-06 14:33:37 +0530

    Add files via upload
    JS Updated File.

---
+++ JavaScript-Template-Literals.js
@@ -0,0 +1,42 @@
+/*
+Synonyms:
+
+Template Literals
+Template Strings
+String Templates
+Back-Tics Syntax
+
+*/
+
+//Back-Tics Syntax
+
+/* 
+    Template Literals use back-ticks (``) rather than the quotes ("") to define a string:
+*/
+
+// Example 1
+let text = `Hello World!`;
+console.log(text);
+
+//Example 2 
+// With template literals, you can use both single and double quotes inside a string:
+
+let text2 = `He's often called "Johnny"`;
+console.log(text2);
+
+// Interpolation
+// Template literals provide an easy way to interpolate variables and expressions into strings. The method is called string interpolation. The syntax is:
+// Template literals allow variables in strings:
+// The syntax is: ${variable}
+
+// Variable Substitutions
+var templateLiterals = "template literals";
+document.write(`This is the example of ${templateLiterals}`);
+ 
+
+
+
+
+
+
+
